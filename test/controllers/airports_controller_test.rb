8b1b40defb0d107693f08b761027683371f9936e
raducioplea <radu.cioplea@gmail.com>
2018-01-08 15:11:57 +0000

    ver 4 commit

--- test/controllers/airports_controller_test.rb
+++ test/controllers/airports_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class AirportsControllerTest < ActionDispatch::IntegrationTest
-  test "should get start" do
-    get airports_start_url
+  test "should get contact" do
+    get airports_contact_url
     assert_response :success
   end
 
